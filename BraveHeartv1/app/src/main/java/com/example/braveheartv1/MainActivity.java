6886eae6971fab04b3666c38f69663a59182406b
Alejandro Ambroj <162425219+Cano9856@users.noreply.github.com>
2024-10-07 08:48:00 +0200

    braveheartv2 completed

--- BraveHeartv1/app/src/main/java/com/example/braveheartv1/MainActivity.java
+++ BraveHeartv1/app/src/main/java/com/example/braveheartv1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.braveheartv1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,36 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btn4;
+        private Button btn5;
+        private Button btn6;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        inicializarComponentes();
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Botón 1!!!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
+        private void inicializarComponentes(){
+            btn1 = findViewById(R.id.btn1);
+            btn2 = findViewById(R.id.btn2);
+            btn3 = findViewById(R.id.btn3);
+            btn4 = findViewById(R.id.btn4);
+            btn5 = findViewById(R.id.btn5);
+            btn6 = findViewById(R.id.btn6);
+        }
 }
